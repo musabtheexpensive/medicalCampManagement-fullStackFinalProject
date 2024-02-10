d35468b7747af0866310da736b471285a929ce6f
Musab Al Mahi <musabalmahi53@gmail.com>
2024-02-10 17:18:47 +0600

    fixed pagination related bug

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -25,12 +25,14 @@ const Home = () => {
   const axios = UseAxiosPublic();
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+  const limit = 4;
   // const [sortBy, setSortBy] = useState("mostRegistered");
   // const camps = useLoaderData();
 
   const getCamps = async () => {
     const res = await axios.get(
-      `/allCamps?sortField=price&sortOrder=${price}&category=${category}`
+      `/allCamps?sortField=price&sortOrder=${price}&category=${category}&page=${page}&limit=${limit}`
     );
     return res;
   };
@@ -41,9 +43,20 @@ const Home = () => {
     isError,
     error,
   } = useQuery({
-    queryKey: ["camp", price, category],
+    queryKey: ["camp", price, category, page],
     queryFn: getCamps,
   });
+
+  const handlePrevious = () => {
+    if (page > 1) setPage(page - 1);
+  };
+  const handleNext = () => {
+    if (page < totalPage) setPage(page + 1);
+  };
+
+  const totalPage = Math.ceil(camps?.data?.total / limit);
+  console.log(totalPage);
+
   if (isError) {
     return <p>Something went Wrong: {error}</p>;
   }
@@ -126,10 +139,44 @@ const Home = () => {
       ) : (
         <div className="grid md:grid-cols-2 gap-4">
           {camps?.data?.result?.map((item) => (
-            <CampCard key={item?.id} camp={item}/>
+            <CampCard key={item?.id} camp={item} />
           ))}
         </div>
       )}
+      <div className="mb-4 flex justify-center ">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="join float-right border-2 border-neutral-200 mt-8">
+            <button
+              onClick={handlePrevious}
+              className="join-item btn btn-ghost"
+            >
+              «
+            </button>
+            {Array.from({ length: totalPage })
+            .map((item, index) => {
+              const pageNumber = index + 1;
+              return (
+                <button
+                  key={pageNumber}
+                  onClick={() => setPage(pageNumber)}
+                  className={`${
+                    pageNumber === page
+                      ? "join-item btn text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 "
+                      : "join-item btn "
+                  }`}
+                >
+                  {pageNumber}
+                </button>
+              );
+            })}
+            <button onClick={handleNext} className="join-item btn btn-ghost">
+              »
+            </button>
+          </div>
+        )}
+      </div>
       <Link to="/availableCamp">
         <button className="m-7 w-11/12 justify-items-center btn lg:ml-16  text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5  me-2 mb-2">
           See All Camps
